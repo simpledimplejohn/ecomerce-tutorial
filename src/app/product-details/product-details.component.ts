98eecfc8d96c03e666aff0bda62acd04055a12ac
Simple Dimple John <simpledimplejohn@gmail.com>
2022-03-11 12:06:42 -0800

    add addToCart method

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product, products } from '../proudcts';
+import { CartService } from '../service/cart.service';
+
 
 @Component({
   selector: 'app-product-details',
@@ -12,7 +14,16 @@ export class ProductDetailsComponent implements OnInit {
   product: Product | undefined;
 
   // ActivatedRoute needs to be injected here
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+  ) { }
+
+  // takes in the current product, adds to the array using the service method
+  addtoCart(product: Product) {
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart');
+  }
 
   ngOnInit(): void {
 
@@ -21,10 +32,7 @@ export class ProductDetailsComponent implements OnInit {
     const productIdFromRoute = Number(routeParams.get('productId'));
 
     // find  the product based on that route
-    this.product = products.find(product => product.id === productIdFromRoute)
-
-
-
+    this.product = products.find(product => product.id === productIdFromRoute);
   }
 
 }
